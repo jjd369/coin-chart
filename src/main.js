51ed0e5e3f4785246c445d98ac55e39179f96b9d
jjd369 <ghana369@gmail.com>
2021-09-27 01:10:32 +0900

    perfect scrollbar 추가

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import store from './store'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'reset-css'
+import PerfectScrollbar from 'vue2-perfect-scrollbar'
+import 'vue2-perfect-scrollbar/dist/vue2-perfect-scrollbar.css'
 
-Vue.config.productionTip = false
+Vue.use(PerfectScrollbar)
+
 Vue.use(Element)
+Vue.config.productionTip = false
 
 new Vue({
   router,
