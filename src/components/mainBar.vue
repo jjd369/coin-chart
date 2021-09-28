13d50dac2f2e3481b6d382269fb0f93dd9d68d52
jjd369 <ghana369@gmail.com>
2021-09-29 00:18:08 +0900

    mainBar.vue 추가

---
+++ src/components/mainBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div
+      ><span>{{ FSYM }} / {{ TSYM }}</span></div
+    >
+    <div>
+      {{ displayTicker.P }}
+    </div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+export default {
+  data() {
+    return {
+      coin_info: {},
+    }
+  },
+  computed: {
+    ...mapState('asset', ['FSYM']),
+    ...mapState('asset', ['TSYM']),
+    ...mapGetters('socket', ['displayTicker']),
+  },
+  methods: {
+    fliter_ticker(val) {},
+  },
+}
+</script>
+
+<style></style>
