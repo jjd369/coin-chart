74c8cfa4bea5586b89d9a736f8f12319a946b380
jjd369 <ghana369@gmail.com>
2021-09-30 01:19:32 +0900

    mainBar.vue socket 값 가져오기

--- src/components/mainBar.vue
+++ src/components/mainBar.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <div
-      ><span>{{ FSYM }} / {{ TSYM }}</span></div
-    >
-    <div>
-      {{ displayTicker.P }}
+      ><span>{{ FSYM }} / {{ TSYM }}</span>
+      <span>{{ displayTrade.P }}</span>
     </div>
-    <div></div>
+    <div>
+      <div>
+        <span>24h Change </span>
+        <span></span>
+      </div>
+      <div><span>24h High </span> <span></span></div>
+      <div><span>24h Low </span> <span></span></div>
+      <div
+        ><span>24h Volume({{ FSYM }}) </span>
+        <span>{{ coin_info.VOLUME24HOUR }}</span></div
+      >
+      <div
+        ><span>24h Volume({{ TSYM }}) </span>
+        <span>{{ coin_info.VOLUME24HOURTO }}</span></div
+      >
+    </div>
   </div>
 </template>
 
@@ -22,6 +35,15 @@ export default {
     ...mapState('asset', ['FSYM']),
     ...mapState('asset', ['TSYM']),
     ...mapGetters('socket', ['displayTicker']),
+    ...mapGetters('socket', ['displayTrade']),
+  },
+  watch: {
+    displayTicker(val) {
+      if (val.FROMSYMBOL !== this.FSYM) return
+      if (val.TOSYMBOL !== this.TSYM) return
+
+      this.coin_info = val
+    },
   },
   methods: {
     fliter_ticker(val) {},
