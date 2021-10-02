aded64c19dd17f3e386c4eaf0686b306d48ab6e9
jjd369 <ghana369@gmail.com>
2021-10-03 00:59:17 +0900

    vuex common.js  추가

---
+++ src/store/modules/common.js
@@ -0,0 +1,12 @@
+export const state = {
+  loading: true,
+}
+export const mutations = {
+  ON_LOADING(state) {
+    state.loading = true
+  },
+  OFF_LOADING(state) {
+    state.loading = false
+  },
+}
+export const actions = {}
