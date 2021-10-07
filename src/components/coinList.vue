71fe1dd27d6a785ae8e319dd51961d84f16f398b
jjd369 <ghana369@gmail.com>
2021-10-07 22:29:53 +0900

    css 수정

--- src/components/coinList.vue
+++ src/components/coinList.vue
@@ -4,6 +4,7 @@
       <el-input placeholder="search" v-model="search_coin"></el-input>
     </div>
     <el-tabs type="card" v-model="activeName" @tab-click="tabClick">
+      <!-- 관심 목록 -->
       <el-tab-pane name="like">
         <span slot="label"><i class="el-icon-star el-icon-star-on"></i></span>
         <div :class="$style.tabContentWrap">
@@ -20,21 +21,70 @@
               <span>Change</span>
             </div>
             <ul :class="$style.contentWrap">
-              <perfect-scrollbar>
+              <perfect-scrollbar :class="$style.ps">
                 <template v-for="(val, index) in c_like_list">
                   <li :class="$style.listWrap" :key="index">
-                    <span>
+                    <div>
                       <i
                         :class="[
                           { 'el-icon-star-on': val.like },
                           'el-icon-star',
                         ]"
-                        @click="likeHandler(val)"
+                        @click="favoritHandler(val)"
                       ></i>
-                      {{ val.FROMSYMBOL }}/{{ TSYM }}
-                    </span>
-                    <span
+                    </div>
+                    <div
+                      :class="$style.listItem"
                       @click="changeAsset(val.FROMSYMBOL, val.TOSYMBOL)"
+                    >
+                      <span> {{ val.FROMSYMBOL }}/{{ val.TOSYMBOL }} </span>
+                      <span
+                        :class="val.FLAGS === '1' ? $style.green : $style.red"
+                      >
+                        {{ calculatePrice(val.PRICE) }}
+                      </span>
+                      <span
+                        :class="
+                          calculateChangeDay(val.OPENDAY, val.PRICE) > 0
+                            ? $style.green
+                            : $style.red
+                        "
+                        >{{
+                          calculateChangeDay(val.OPENDAY, val.PRICE) + '%'
+                        }}</span
+                      >
+                    </div>
+                  </li>
+                </template>
+              </perfect-scrollbar>
+            </ul>
+          </div>
+        </div>
+      </el-tab-pane>
+      <!-- BTC -->
+      <el-tab-pane label="BTC" name="BTC">
+        <div :class="$style.tabContentWrap">
+          <div :class="$style.titleWrap">
+            <span>Pair</span>
+            <span>Price</span>
+            <span>Change</span>
+          </div>
+          <ul :class="$style.contentWrap">
+            <perfect-scrollbar :class="$style.ps">
+              <template v-for="(val, index) in c_filter_search_list">
+                <li :class="$style.listWrap" :key="index">
+                  <div>
+                    <i
+                      :class="[{ 'el-icon-star-on': val.like }, 'el-icon-star']"
+                      @click="favoritHandler(val)"
+                    ></i>
+                  </div>
+                  <div
+                    :class="$style.listItem"
+                    @click="changeAsset(val.FROMSYMBOL, val.TOSYMBOL)"
+                  >
+                    <span> {{ val.FROMSYMBOL }}/{{ val.TOSYMBOL }} </span>
+                    <span
                       :class="val.FLAGS === '1' ? $style.green : $style.red"
                     >
                       {{ calculatePrice(val.PRICE) }}
@@ -49,53 +99,14 @@
                         calculateChangeDay(val.OPENDAY, val.PRICE) + '%'
                       }}</span
                     >
-                  </li>
-                </template>
-              </perfect-scrollbar>
-            </ul>
-          </div>
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="BTC" name="BTC">
-        <div :class="$style.tabContentWrap">
-          <div :class="$style.titleWrap">
-            <span>Pair</span>
-            <span>Price</span>
-            <span>Change</span>
-          </div>
-          <ul :class="$style.contentWrap">
-            <perfect-scrollbar>
-              <template v-for="(val, index) in c_search_filter_list">
-                <li :class="$style.listWrap" :key="index">
-                  <span>
-                    <i
-                      :class="[{ 'el-icon-star-on': val.like }, 'el-icon-star']"
-                      @click="likeHandler(val)"
-                    ></i>
-                    {{ val.FROMSYMBOL }}/{{ TSYM }}
-                  </span>
-                  <span
-                    @click="changeAsset(val.FROMSYMBOL, val.TOSYMBOL)"
-                    :class="val.FLAGS === '1' ? $style.green : $style.red"
-                  >
-                    {{ calculatePrice(val.PRICE) }}
-                  </span>
-                  <span
-                    :class="
-                      calculateChangeDay(val.OPENDAY, val.PRICE) > 0
-                        ? $style.green
-                        : $style.red
-                    "
-                    >{{
-                      calculateChangeDay(val.OPENDAY, val.PRICE) + '%'
-                    }}</span
-                  >
+                  </div>
                 </li>
               </template>
             </perfect-scrollbar>
           </ul>
         </div>
       </el-tab-pane>
+      <!-- ETH -->
       <el-tab-pane label="ETH" name="ETH">
         <div :class="$style.tabContentWrap">
           <div :class="$style.titleWrap">
@@ -104,39 +115,87 @@
             <span>Change</span>
           </div>
           <ul :class="$style.contentWrap">
-            <perfect-scrollbar>
-              <template v-for="(val, index) in c_search_filter_list">
+            <perfect-scrollbar :class="$style.ps">
+              <template v-for="(val, index) in c_filter_search_list">
                 <li :class="$style.listWrap" :key="index">
-                  <span>
+                  <div>
                     <i
                       :class="[{ 'el-icon-star-on': val.like }, 'el-icon-star']"
-                      @click="likeHandler(val)"
+                      @click="favoritHandler(val)"
                     ></i>
-                    {{ val.FROMSYMBOL }}/{{ TSYM }}
-                  </span>
-                  <span
+                  </div>
+                  <div
+                    :class="$style.listItem"
                     @click="changeAsset(val.FROMSYMBOL, val.TOSYMBOL)"
-                    :class="val.FLAGS === '1' ? $style.green : $style.red"
                   >
-                    {{ calculatePrice(val.PRICE) }}
-                  </span>
-                  <span
-                    :class="
-                      calculateChangeDay(val.OPENDAY, val.PRICE) > 0
-                        ? $style.green
-                        : $style.red
-                    "
-                    >{{
-                      calculateChangeDay(val.OPENDAY, val.PRICE) + '%'
-                    }}</span
-                  >
+                    <span> {{ val.FROMSYMBOL }}/{{ val.TOSYMBOL }} </span>
+                    <span
+                      :class="val.FLAGS === '1' ? $style.green : $style.red"
+                    >
+                      {{ calculatePrice(val.PRICE) }}
+                    </span>
+                    <span
+                      :class="
+                        calculateChangeDay(val.OPENDAY, val.PRICE) > 0
+                          ? $style.green
+                          : $style.red
+                      "
+                      >{{
+                        calculateChangeDay(val.OPENDAY, val.PRICE) + '%'
+                      }}</span
+                    >
+                  </div>
                 </li>
               </template>
             </perfect-scrollbar>
           </ul>
         </div>
       </el-tab-pane>
-      <el-tab-pane label="USDT" name="USDT">Task</el-tab-pane>
+      <!-- USDT -->
+      <el-tab-pane label="USDT" name="USDT"
+        ><div :class="$style.tabContentWrap">
+          <div :class="$style.titleWrap">
+            <span>Pair</span>
+            <span>Price</span>
+            <span>Change</span>
+          </div>
+          <ul :class="$style.contentWrap">
+            <perfect-scrollbar :class="$style.ps">
+              <template v-for="(val, index) in c_filter_search_list">
+                <li :class="$style.listWrap" :key="index">
+                  <div>
+                    <i
+                      :class="[{ 'el-icon-star-on': val.like }, 'el-icon-star']"
+                      @click="favoritHandler(val)"
+                    ></i>
+                  </div>
+                  <div
+                    :class="$style.listItem"
+                    @click="changeAsset(val.FROMSYMBOL, val.TOSYMBOL)"
+                  >
+                    <span> {{ val.FROMSYMBOL }}/{{ val.TOSYMBOL }} </span>
+                    <span
+                      :class="val.FLAGS === '1' ? $style.green : $style.red"
+                    >
+                      {{ calculatePrice(val.PRICE) }}
+                    </span>
+                    <span
+                      :class="
+                        calculateChangeDay(val.OPENDAY, val.PRICE) > 0
+                          ? $style.green
+                          : $style.red
+                      "
+                      >{{
+                        calculateChangeDay(val.OPENDAY, val.PRICE) + '%'
+                      }}</span
+                    >
+                  </div>
+                </li>
+              </template>
+            </perfect-scrollbar>
+          </ul>
+        </div></el-tab-pane
+      >
     </el-tabs>
   </div>
 </template>
@@ -149,6 +208,7 @@ import map from 'lodash/map'
 import find from 'lodash/find'
 import findIndex from 'lodash/findIndex'
 import Decimal from 'decimal.js'
+import clone from 'lodash/clone'
 
 export default {
   data() {
@@ -156,44 +216,55 @@ export default {
       max: 1,
       coin_list: [],
       search_coin: '',
-      activeName: 'BTC',
+      activeName: 'USDT',
     }
   },
   computed: {
     ...mapState('asset', ['TSYM']),
     ...mapState('asset', ['FSYM']),
+    ...mapState('asset', ['fsyms_list']),
     ...mapState('asset', ['assets_full_data']),
     ...mapGetters('socket', ['displayTicker']),
-    c_search_filter_list() {
-      return filter(
-        this.coin_list,
-        (el) =>
-          !this.search_coin ||
-          el.FROMSYMBOL.toLowerCase().includes(
-            this.search_coin.toLowerCase()
-          ) ||
-          el.TOSYMBOL.toLowerCase().includes(this.search_coin.toLowerCase())
-      )
+    c_filter_tab_list() {
+      return filter(this.assets_full_data, { TOSYMBOL: this.activeName })
+    },
+    c_filter_search_list() {
+      return !this.search_coin
+        ? this.c_filter_tab_list
+        : filter(
+            this.assets_full_data,
+            (el) =>
+              !this.search_coin ||
+              `${el.FROMSYMBOL.toLowerCase()}/${el.TOSYMBOL.toLowerCase()}`.includes(
+                this.search_coin.toLowerCase()
+              )
+          )
+      // return filter(
+      //   this.c_filter_tab_list,
+      //   (el) =>
+      //     !this.search_coin ||
+      //     `${el.FROMSYMBOL.toLowerCase()}/${el.TOSYMBOL.toLowerCase()}`.includes(
+      //       this.search_coin.toLowerCase()
+      //     )
+      // )
     },
     c_like_list() {
-      return filter(this.coin_list, { like: true })
+      return filter(this.assets_full_data, { like: true })
     },
   },
   watch: {
     displayTicker(newValue) {
-      let find_asset = find(this.coin_list, {
+      let find_asset = find(this.assets_full_data, {
         FROMSYMBOL: newValue.FROMSYMBOL,
+        TOSYMBOL: newValue.TOSYMBOL,
       })
+
       if (find_asset === undefined) return
       if (!newValue.PRICE) return
 
       find_asset.PRICE = newValue.PRICE
       find_asset.FLAGS = newValue.FLAGS.toString()
-
-      let find_asset_index = findIndex(this.coin_list, {
-        FROMSYMBOL: newValue.FROMSYMBOL,
-      })
-      this.coin_list.splice(find_asset_index, 1, find_asset)
+      this.$store.dispatch('asset/updateSymbolFullData', find_asset)
     },
   },
   methods: {
@@ -202,6 +273,7 @@ export default {
       setTimeout(() => {
         this.$store.commit('common/OFF_LOADING')
       }, 2000)
+      console.log(FSYM, TSYM)
       this.$store.dispatch('asset/updateSymbol', { FSYM, TSYM })
     },
     calculatePrice(price) {
@@ -210,19 +282,25 @@ export default {
     calculateChangeDay(openDay, price) {
       return Decimal.sub(price, openDay).div(openDay).mul(100).toFixed(2)
     },
-    likeHandler(obj) {
-      let tlqkf = this.coin_list.findIndex((el) => el === obj)
-      obj.like = !obj.like
-      this.coin_list.splice(tlqkf, 1, obj)
+    favoritHandler(obj) {
+      this.$store.dispatch('asset/updateFavorit', obj)
     },
     tabClick() {
       if (this.activeName === 'like') return
-      if (this.activeName === this.TSYM) return
-      this.$store.dispatch('asset/updateSymbol', {
-        FSYM: this.FSYM,
-        TSYM: this.activeName,
-      })
-      this.$store.dispatch('asset/getSymbolsFullData')
+
+      let sub_remove_obj = {
+        type: 2,
+        fsyms: this.fsyms_list,
+        tsym: this.TSYM,
+      }
+      let sub_add_obj = {
+        type: 2,
+        fsyms: this.fsyms_list,
+        tsym: this.activeName,
+      }
+
+      this.$store.dispatch('socket/deleteChannelString', sub_remove_obj)
+      this.$store.dispatch('socket/addChannelString', sub_add_obj)
     },
     Decimal,
   },
@@ -239,6 +317,9 @@ export default {
   .noLikeList {
     padding-top: 30px;
   }
+  .ps {
+    height: 500px;
+  }
 }
 .coinListWrap {
   .searchWrap {
@@ -249,12 +330,24 @@ export default {
   .titleWrap {
     display: flex;
     align-items: center;
+    padding: 10px;
     span {
       display: flex;
-      flex: 1 1 0%;
-      padding-left: 50px;
-      padding-top: 10px;
-      padding-bottom: 10px;
+      &:nth-child(1) {
+        flex: 5 1 0px;
+        justify-content: flex-start;
+        min-width: 120px;
+      }
+      &:nth-child(2) {
+        flex: 3 1 0px;
+        justify-content: flex-end;
+        min-width: 50px;
+      }
+      &:nth-child(3) {
+        flex: 3 1 0px;
+        justify-content: flex-end;
+        min-width: 60px;
+      }
     }
   }
   .listWrap {
@@ -263,26 +356,43 @@ export default {
     &:hover {
       cursor: pointer;
     }
+    padding: 0 5px 15px 5px;
+    .listItem {
+      display: flex;
+      flex: 1 1 0;
+      padding: 0 5px;
+    }
     span {
       display: flex;
-      flex: 1 1 0%;
-      // padding-left: 50px;
-      padding-top: 10px;
-      padding-bottom: 10px;
+      &:nth-child(1) {
+        flex: 5 1 0px;
+        justify-content: flex-start;
+        min-width: 120px;
+      }
+      &:nth-child(2) {
+        flex: 3 1 0px;
+        justify-content: flex-end;
+        min-width: 50px;
+      }
+      &:nth-child(3) {
+        flex: 3 1 0px;
+        justify-content: flex-end;
+        min-width: 60px;
+      }
     }
     .green {
-      color: #0ecb81;
+      color: $green;
     }
     .red {
-      color: #f6465d;
+      color: $red;
     }
   }
 }
 </style>
-<style lang="scss">
-.ps {
+<style>
+/* .ps {
   height: 500px;
-}
+} */
 </style>
 <style>
 .el-icon-star {
